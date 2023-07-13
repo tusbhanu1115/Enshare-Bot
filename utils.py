48ee1ed1679c2b820a7dd1dce0f48aa339e445b3
tusbhanu1115 <125240526+tusbhanu1115@users.noreply.github.com>
2023-07-13 12:39:28 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -23,16 +23,3 @@ def extract_nums(url):
         num2 = int(match.group(1))
 
     return num1, num2
-
-def forward_posts(bot, chat_id, start_id, end_id):
-    message_ids = []
-    for post_id in range(start_id, end_id + 1):
-        try:
-            message = bot.forward_message(chat_id, CHAT_ID, post_id)
-            message_ids.append(message.message_id)
-        except telebot.apihelper.ApiTelegramException as e:
-            print(f"Error forwarding message: {e}")
-    
-    # Sending a single message that includes all the forwarded posts
-    if message_ids:
-        bot.send_message(chat_id, "\n".join([f"https://t.me/{CHAT_ID}/{message_id}" for message_id in message_ids]))
