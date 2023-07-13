19c8446cc83d84d2daf1d738ccf38673152d2dd6
tusbhanu1115 <125240526+tusbhanu1115@users.noreply.github.com>
2023-07-13 16:10:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,40 @@
-import telebot
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Command
 from constant import *
 from start_commands import *
 from utils import *
 from btns import *
 from msgs import *
-from http.server import BaseHTTPRequestHandler, HTTPServer
 
-bot = telebot.TeleBot(API_TOKEN)
+logging.basicConfig(level=logging.INFO)
 
-@bot.message_handler(commands=['start'])
-def handle_start_wrapper(message):
-    handle_start(bot, message)
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+dp.middleware.setup(LoggingMiddleware())
 
-@bot.callback_query_handler(func=lambda call: True)
-def handle_btn_click_wrapper(call):
-    handle_button_click(bot,call)
 
-@bot.message_handler(func=lambda message: message.chat.id in user_state and user_state[message.chat.id] == STATE_WAITING_BATCH_LINKS)
-def handle_batch_wrapper(message):
-    handle_batch_links(bot,message)
+@dp.message_handler(Command("start"))
+async def handle_start(message: types.Message):
+    async def handle_start_wrapper(message):
+        await handle_start(bot,message)
 
-class WebhookHandler(BaseHTTPRequestHandler):
-    def _set_response(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
 
-    def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
-        update_data = self.rfile.read(content_length)
-        update = telebot.types.Update.de_json(update_data.decode('utf-8'))
-        bot.process_new_updates([update])
-        self._set_response()
+@dp.callback_query_handler()
+async def handle_btn_click(call: types.CallbackQuery):
+    async handle_btn_click_wrapper(call):
+        await handle_button_click(bot,call)
 
-def run_server():
-    server_address = ('', 3000)  # Update port number if needed
-    httpd = HTTPServer(server_address, WebhookHandler)
-    httpd.serve_forever()
+
+@dp.message_handler(state='*', func=lambda message: message.chat.id in user_state and user_state[message.chat.id] == STATE_WAITING_BATCH_LINKS)
+async def handle_batch(message: types.Message, state: FSMContext):
+    async handle_batch_wrapper(message, state):
+        await handle_batch_links(bot,message)
+
 
 if __name__ == '__main__':
-    bot.remove_webhook()
-    bot.set_webhook(url=WEBHOOK_URL)
+    from aiogram import executor
 
-    run_server()
+    executor.start_polling(dp, skip_updates=True)
