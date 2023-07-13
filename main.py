deb41171e25d3358b434c24c31d949ad432309f0
tusbhanu1115 <125240526+tusbhanu1115@users.noreply.github.com>
2023-07-13 16:34:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,22 @@
-import logging
-from aiogram import Bot, Dispatcher, types
-from aiogram.contrib.middlewares.logging import LoggingMiddleware
-from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters import Command
+import telebot
 from constant import *
 from start_commands import *
 from utils import *
 from btns import *
 from msgs import *
 
-logging.basicConfig(level=logging.INFO)
+bot = telebot.TeleBot(API_TOKEN)
 
-bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
-dp.middleware.setup(LoggingMiddleware())
+@bot.message_handler(commands=['start'])
+def handle_start_wrapper(message):
+    handle_start(bot, message)
 
+@bot.callback_query_handler(func=lambda call: True)
+def handle_btn_click_wrapper(call):
+    handle_button_click(bot,call)
 
-@dp.message_handler(commands=['start'])
-async def handle_start(message: types.Message):
-    # Send six messages to the user
-    for i in range(1, 7):
-        await message.answer(f"This is message {i}")
+@bot.message_handler(func=lambda message: message.chat.id in user_state and user_state[message.chat.id] == STATE_WAITING_BATCH_LINKS)
+def handle_batch_wrapper(message):
+    handle_batch_links(bot,message)
 
-        # Add a small delay between each message (optional)
-        await asyncio.sleep(1)
-
-if __name__ == '__main__':
-    from aiogram import executor
-
-    executor.start_polling(dp, skip_updates=True)
+bot.polling()
