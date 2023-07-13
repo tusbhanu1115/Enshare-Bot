270876e76d7edab23a843e7bf9c5826ff6d913d4
tusbhanu1115 <125240526+tusbhanu1115@users.noreply.github.com>
2023-07-13 13:27:50 +0530

    Update start_commands.py

--- start_commands.py
+++ start_commands.py
@@ -1,18 +1,30 @@
 from utils import *
 from telebot import types
-from collections import deque
-import asyncio
-from aiogram import Bot, Dispatcher, types
 
-async def send_multiple_messages(chat_id):
-    messages = ["Message 1", "Message 2", "Message 3"]
+def handle_start(bot,message):
+    if message.text.startswith('/start'):
+        split_text = message.text.split('/start ')
+        if len(split_text) > 1:
+            start_value = ' '.join(split_text[1:])
+            start_id, end_id = extract_nums(start_value)
+            forward_posts(bot,message.chat.id, start_id, end_id)
+        else:
+            if message.from_user.id == OWNER_ID:
+                keyboard = types.InlineKeyboardMarkup()
 
-    for message in messages:
-        await bot.send_message(chat_id, message)
-        await asyncio.sleep(1)  # Add a delay between messages (optional)
+                aboutme = types.InlineKeyboardButton("😊 About Me", callback_data='about_me')
+                close = types.InlineKeyboardButton("🔒 Close", callback_data='close')
+                genbatch = types.InlineKeyboardButton("⚡️ Generate Batch ⚡️", callback_data='genbatch')
 
-async def handle_start(bot,message: types.Message):
-    # Start sending messages asynchronously
-    await send_multiple_messages(message.chat.id)
+                keyboard.add(aboutme, close)
+                keyboard.add(genbatch)
 
+                # Welcome message sent to users
+                WELCOME_MSG = "<b>Hello {name}.</b> I can store private files in a specified channel, and other users can access them from a special link."
 
+                welcome_msg = WELCOME_MSG.format(name=message.from_user.first_name)
+                # Sending the initial message
+                bot.send_message(message.chat.id, welcome_msg, reply_markup=keyboard, parse_mode='HTML')
+            else:
+                unauthorized_msg = "<b>❌ Don't send me messages directly. You can only access me using special links provided on our channels.</b>"
+                bot.send_message(message.chat.id, unauthorized_msg, parse_mode='HTML')
