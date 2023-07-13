4d1b837d043f04c7c4e87eb752da5b39a080a90d
tusbhanu1115 <125240526+tusbhanu1115@users.noreply.github.com>
2023-07-13 17:32:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from start_commands import *
 from utils import *
 from btns import *
 from msgs import *
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
 bot = telebot.TeleBot(API_TOKEN)
 
@@ -19,4 +20,26 @@ def handle_btn_click_wrapper(call):
 def handle_batch_wrapper(message):
     handle_batch_links(bot,message)
 
-bot.polling()
+class WebhookHandler(BaseHTTPRequestHandler):
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        update_data = self.rfile.read(content_length)
+        update = telebot.types.Update.de_json(update_data.decode('utf-8'))
+        bot.process_new_updates([update])
+        self._set_response()
+
+def run_server():
+    server_address = ('', 3000)  # Update port number if needed
+    httpd = HTTPServer(server_address, WebhookHandler)
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    bot.remove_webhook()
+    bot.set_webhook(url=WEBHOOK_URL)
+
+    run_server()
