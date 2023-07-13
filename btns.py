6e3cb3d59d08fcf2877e9f3ac574e3a40fba1376
tusbhanu1115 <125240526+tusbhanu1115@users.noreply.github.com>
2023-07-13 17:26:16 +0530

    Update btns.py

--- btns.py
+++ btns.py
@@ -2,11 +2,8 @@ import pyperclip
 from utils import *
 from telebot import types
 
-@bot.callback_query_handler(func=lambda call: True)
-async def handle_btn_click(call):
-    await handle_button_click(call)
+def handle_button_click(bot,call):
 
-async def handle_button_click(call):
     if call.data == 'about_me':
         # Creating the custom keyboard with the "Back" button
         keyboard = types.InlineKeyboardMarkup()
@@ -15,15 +12,15 @@ async def handle_button_click(call):
 
         text = "Created By: Tushar Bhanushali\nLanguage: Python"
 
-        await bot.edit_message_text(text, call.message.chat.id, call.message.message_id, reply_markup=keyboard)
+        bot.edit_message_text(text, call.message.chat.id, call.message.message_id, reply_markup=keyboard)
 
     elif call.data == 'close':
         # Delete the message sent by the bot
-        await bot.delete_message(call.message.chat.id, call.message.message_id)
+        bot.delete_message(call.message.chat.id, call.message.message_id)
 
         # Delete the replied-to message if available
         if call.message.reply_to_message:
-            await bot.delete_message(call.message.chat.id, call.message.reply_to_message.message_id)
+            bot.delete_message(call.message.chat.id, call.message.reply_to_message.message_id)
 
     elif call.data == 'back':
         # Creating the custom keyboard with the "About Me" button
@@ -36,18 +33,18 @@ async def handle_button_click(call):
         keyboard.add(genbatch)
 
         # Sending the main start message with the "About Me" button
-        await bot.edit_message_text(WELCOME_MSG, call.message.chat.id, call.message.message_id, reply_markup=keyboard)
+        bot.edit_message_text(WELCOME_MSG, call.message.chat.id, call.message.message_id, reply_markup=keyboard)
 
     elif call.data == 'genbatch':
-        await bot.send_message(call.message.chat.id, "Send me Batch Start & End Link (separated by '-')")
+        bot.send_message(call.message.chat.id, "Send me Batch Start & End Link (separated by '-')")
         user_state[call.message.chat.id] = STATE_WAITING_BATCH_LINKS
-
+        
     elif call.data == 'copy':
         unique_link = user_state.get(call.message.chat.id)
         if unique_link:
             pyperclip.copy(unique_link)  # Copy the unique link to the clipboard
 
             copy_msg = "<b>✅ Sharable Batch link copied to clipboard.</b>"
-            await bot.send_message(call.message.chat.id, copy_msg, parse_mode='HTML')
+            bot.send_message(call.message.chat.id, copy_msg, parse_mode='HTML')
         else:
-            await bot.send_message(call.message.chat.id, "Unable to copy the unique link. Please generate a valid batch first.")
+            bot.send_message(call.message.chat.id, "Unable to copy the unique link. Please generate a valid batch first.")
