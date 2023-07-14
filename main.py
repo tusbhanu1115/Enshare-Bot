326c73d41ccc8bb7d2043c020619d8e98add2552
tusbhanu1115 <125240526+tusbhanu1115@users.noreply.github.com>
2023-07-14 13:09:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,22 +5,21 @@ from utils import *
 from btns import *
 from msgs import *
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import asyncio
 
 bot = telebot.TeleBot(API_TOKEN)
 
 @bot.message_handler(commands=['start'])
-async def handle_start_wrapper(message):
-    await handle_start(bot, message)
+def handle_start_wrapper(message):
+    handle_start(bot, message)
 
 @bot.callback_query_handler(func=lambda call: True)
 def handle_btn_click_wrapper(call):
-    handle_button_click(bot, call)
+    handle_button_click(bot,call)
 
 @bot.message_handler(func=lambda message: message.chat.id in user_state and user_state[message.chat.id] == STATE_WAITING_BATCH_LINKS)
 def handle_batch_wrapper(message):
-    handle_batch_links(bot, message)
-
+    handle_batch_links(bot,message)
+    
 class WebhookHandler(BaseHTTPRequestHandler):
     def _set_response(self):
         self.send_response(200)
@@ -31,11 +30,8 @@ class WebhookHandler(BaseHTTPRequestHandler):
         content_length = int(self.headers['Content-Length'])
         update_data = self.rfile.read(content_length)
         update = telebot.types.Update.de_json(update_data.decode('utf-8'))
-        asyncio.run(self.process_update(bot, update))  # Use asyncio.run to await the coroutine
+        bot.process_new_updates([update])
         self._set_response()
-
-    async def process_update(self, bot, update):
-        await bot.process_new_updates([update])
 
 def run_server():
     server_address = ('', 3000)  # Update port number if needed
