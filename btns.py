e179d555fdfb952b596188a476ec72c39e86fa20
tusbhanu1115 <125240526+tusbhanu1115@users.noreply.github.com>
2023-07-14 15:08:59 +0530

    Update btns.py

--- btns.py
+++ btns.py
@@ -1,50 +1,59 @@
-import pyperclip
+from pyrogram import Client, filters
 from utils import *
-from telebot import types
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-def handle_button_click(bot,call):
+bot = Client(
+    "my_bot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=API_TOKEN
+)
 
-    if call.data == 'about_me':
+@bot.on_callback_query()
+def handle_button_click(client, callback_query):
+
+    if callback_query.data == 'about_me':
         # Creating the custom keyboard with the "Back" button
-        keyboard = types.InlineKeyboardMarkup()
-        back = types.InlineKeyboardButton("👈 Back", callback_data='back')
-        keyboard.add(back)
+        keyboard = InlineKeyboardMarkup(
+            [[InlineKeyboardButton("👈 Back", callback_data='back')]]
+        )
 
         text = "Created By: Tushar Bhanushali\nLanguage: Python"
 
-        bot.edit_message_text(text, call.message.chat.id, call.message.message_id, reply_markup=keyboard)
+        client.edit_message_text(callback_query.message.chat.id, callback_query.message.message_id, text, reply_markup=keyboard)
 
-    elif call.data == 'close':
+    elif callback_query.data == 'close':
         # Delete the message sent by the bot
-        bot.delete_message(call.message.chat.id, call.message.message_id)
+        client.delete_messages(callback_query.message.chat.id, callback_query.message.message_id)
 
         # Delete the replied-to message if available
-        if call.message.reply_to_message:
-            bot.delete_message(call.message.chat.id, call.message.reply_to_message.message_id)
+        if callback_query.message.reply_to_message:
+            client.delete_messages(callback_query.message.chat.id, callback_query.message.reply_to_message.message_id)
 
-    elif call.data == 'back':
+    elif callback_query.data == 'back':
         # Creating the custom keyboard with the "About Me" button
-        keyboard = types.InlineKeyboardMarkup()
-        aboutme = types.InlineKeyboardButton("😊 About Me", callback_data='about_me')
-        close = types.InlineKeyboardButton("🔒 Close", callback_data='close')
-        genbatch = types.InlineKeyboardButton("⚡️ Generate Batch ⚡️", callback_data='genbatch')
-
-        keyboard.add(aboutme, close)
-        keyboard.add(genbatch)
+        keyboard = InlineKeyboardMarkup(
+            [
+                [InlineKeyboardButton("😊 About Me", callback_data='about_me')],
+                [InlineKeyboardButton("🔒 Close", callback_data='close')],
+                [InlineKeyboardButton("⚡️ Generate Batch ⚡️", callback_data='genbatch')]
+            ]
+        )
 
         # Sending the main start message with the "About Me" button
-        bot.edit_message_text(WELCOME_MSG, call.message.chat.id, call.message.message_id, reply_markup=keyboard)
+        client.edit_message_text(callback_query.message.chat.id, callback_query.message.message_id, WELCOME_MSG, reply_markup=keyboard)
 
-    elif call.data == 'genbatch':
-        bot.send_message(call.message.chat.id, "Send me Batch Start & End Link (separated by '-')")
-        user_state[call.message.chat.id] = STATE_WAITING_BATCH_LINKS
-        
-    elif call.data == 'copy':
-        unique_link = user_state.get(call.message.chat.id)
+    elif callback_query.data == 'genbatch':
+        client.send_message(callback_query.message.chat.id, "Send me Batch Start & End Link (separated by '-')")
+        user_state[callback_query.message.chat.id] = STATE_WAITING_BATCH_LINKS
+
+    elif callback_query.data == 'copy':
+        unique_link = user_state.get(callback_query.message.chat.id)
         if unique_link:
-            pyperclip.copy(unique_link)  # Copy the unique link to the clipboard
-
             copy_msg = "<b>✅ Sharable Batch link copied to clipboard.</b>"
-            bot.send_message(call.message.chat.id, copy_msg, parse_mode='HTML')
+            bot.send_message(callback_query.message.chat.id, copy_msg, parse_mode='HTML')
         else:
-            bot.send_message(call.message.chat.id, "Unable to copy the unique link. Please generate a valid batch first.")
+            bot.send_message(callback_query.message.chat.id, "Unable to copy the unique link. Please generate a valid batch first.")
+
+bot.start()
+bot.run()
