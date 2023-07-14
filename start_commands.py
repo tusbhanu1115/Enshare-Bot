7c74c626e8c3bb620c02dc24c011affd1d52e20a
tusbhanu1115 <125240526+tusbhanu1115@users.noreply.github.com>
2023-07-14 15:15:12 +0530

    Update start_commands.py

--- start_commands.py
+++ start_commands.py
@@ -1,30 +1,34 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from utils import *
-from telebot import types
 
-def handle_start(bot,message):
-    if message.text.startswith('/start'):
-        split_text = message.text.split('/start ')
-        if len(split_text) > 1:
-            start_value = ' '.join(split_text[1:])
-            start_id, end_id = extract_nums(start_value)
-            forward_posts(bot,message.chat.id, start_id, end_id)
+bot = Client(
+    "my_bot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=API_TOKEN
+)
+
+@bot.on_message(filters.command(['start']))
+def handle_start_wrapper(client, message):
+    if len(message.command) > 1:
+        start_value = ' '.join(message.command[1:])
+        start_id, end_id = extract_nums(start_value)
+        forward_posts(client, message.chat.id, start_id, end_id)
+    else:
+        if message.from_user.id == OWNER_ID:
+            keyboard = InlineKeyboardMarkup(
+                [
+                    [InlineKeyboardButton("😊 About Me", callback_data='about_me')],
+                    [InlineKeyboardButton("🔒 Close", callback_data='close')],
+                    [InlineKeyboardButton("⚡️ Generate Batch ⚡️", callback_data='genbatch')]
+                ]
+            )
+
+            WELCOME_MSG = "<b>Hello {name}.</b> I can store private files in a specified channel, and other users can access them from a special link."
+
+            welcome_msg = WELCOME_MSG.format(name=message.from_user.first_name)
+            client.send_message(message.chat.id, welcome_msg, reply_markup=keyboard, parse_mode='HTML')
         else:
-            if message.from_user.id == OWNER_ID:
-                keyboard = types.InlineKeyboardMarkup()
-
-                aboutme = types.InlineKeyboardButton("😊 About Me", callback_data='about_me')
-                close = types.InlineKeyboardButton("🔒 Close", callback_data='close')
-                genbatch = types.InlineKeyboardButton("⚡️ Generate Batch ⚡️", callback_data='genbatch')
-
-                keyboard.add(aboutme, close)
-                keyboard.add(genbatch)
-
-                # Welcome message sent to users
-                WELCOME_MSG = "<b>Hello {name}.</b> I can store private files in a specified channel, and other users can access them from a special link."
-
-                welcome_msg = WELCOME_MSG.format(name=message.from_user.first_name)
-                # Sending the initial message
-                bot.send_message(message.chat.id, welcome_msg, reply_markup=keyboard, parse_mode='HTML')
-            else:
-                unauthorized_msg = "<b>❌ Don't send me messages directly. You can only access me using special links provided on our channels.</b>"
-                bot.send_message(message.chat.id, unauthorized_msg, parse_mode='HTML')
+            unauthorized_msg = "<b>❌ Don't send me messages directly. You can only access me using special links provided on our channels.</b>"
+            client.send_message(message.chat.id, unauthorized_msg, parse_mode='HTML')
