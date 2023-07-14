548100c4bed1374859862474c9750341467fdd95
tusbhanu1115 <125240526+tusbhanu1115@users.noreply.github.com>
2023-07-14 12:44:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,16 +9,16 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 bot = telebot.TeleBot(API_TOKEN)
 
 @bot.message_handler(commands=['start'])
-def handle_start_wrapper(message):
-    handle_start(bot, message)
+async def handle_start_wrapper(message):
+    await handle_start(bot, message)
 
 @bot.callback_query_handler(func=lambda call: True)
 def handle_btn_click_wrapper(call):
-    handle_button_click(bot,call)
+    handle_button_click(bot, call)
 
 @bot.message_handler(func=lambda message: message.chat.id in user_state and user_state[message.chat.id] == STATE_WAITING_BATCH_LINKS)
 def handle_batch_wrapper(message):
-    handle_batch_links(bot,message)
+    handle_batch_links(bot, message)
 
 class WebhookHandler(BaseHTTPRequestHandler):
     def _set_response(self):
